efde376381e6c4891a5694aac9edcda04066222b
Level ASMer <zx132435@naver.com>
2017-10-15 23:03:15 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html><head><title>Touched by Unknown</title><link href="./indone10.gif" rel="shotcut icon">
+</head><body bgcolor="black" background="./68vwqc.gif" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onselectstart="return false" ondragstart="return false">
+<center><pre><font color="red">
+            )       )      )      )                  )  
+         ( /(    ( /(   ( /(   ( /(    (  (       ( /(  
+    (    )\())   )\())  )\())  )\())   )\))(   '  )\()) 
+    )\  ((_)\  |((_)\  ((_)\  ((_)\   ((_)()\ )  ((_)\  
+ _ ((_)  _((_) |_ ((_)  _((_)   ((_)  _(())\_)()  _((_) 
+| | | | | \| | | |/ /  | \| |  / _ \  \ \((_)/ / | \| | 
+| |_| | | .` |   ' &lt;   | .` | | (_) |  \ \/\/ /  | .` | 
+ \___/  |_|\_|  _|\_\  |_|\_|  \___/    \_/\_/   |_|\_| 
+                                                        
+</font></pre></center><font color="red">
+<center><marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="80" width="100%" style="width: 100%;"><font color="Red"> _____________________________________ </font></marquee>
+<font size="7" color="red"> Touched by </font><font size="7" color="white"><blink>Unknown</blink></font>
+<marquee behavior="scroll" direction="right" scrollamount="100" scrolldelay="80" width="100%" style="width: 100%;"><font color="white"> _____________________________________ </font></marquee>
+<marquee behavior="scroll" direction="left" scrollamount="5" scrolldelay="20" width="50%" style="width: 50%;"><font face="Courier" color="grey">Big Thank's to : | Muslim Fighter 007 | |Indonesia Fighter Cyber | | Indonesian Black Cyber | | Indonesian Security Down | Bandung Blackhat | Java Defacer Team |  Surabaya BlackHat | BOGORHACKERS FAMILY | Zone Injector | IDCA | IDTT | IDN </font></marquee>
+ <center><div id="layang"><font face="Agency FB" color="red" size="5px"> </font></div></center>
+<footer>
+<center>
+<div class="gaf210imvustylez_youtubebox" style="width:1px;height:1px;overflow:hidden"><iframe width="300" height="300" src="https://www.youtube.com/embed/8A2t_tAjMz8?autoplay=1&amp;loop=1&amp;playlist=8A2t_tAjMz8" frameborder="0" allowfullscreen=""></iframe></div>
+<!--<a href="http://feedjit.com/ir1/b6044547c94c9b9677cd5df9a2a79ce1/">-->
+<!--<img width="0" src="http://feedjit.com/b/b6044547c94c9b9677cd5df9a2a79ce1.png" alt="" border="0" ismap="" hidden="" style="display: none !important;"></a>--></center></footer></center></font></body><whale translate="no"></whale></html>
